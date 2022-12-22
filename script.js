48f056f38eba6e882640f4fdf2fd81ba61179203
Humeyra02 <humeyrasimsek582@gmail.com>
2022-12-22 13:36:02 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+let addToDoButton = document.getElementById('addToDo');
+let pp = document.getElementById('p');
+let inputText = document.getElementById('inputText');
+let clearToDo = document.getElementById('clearToDo');
+
+addToDoButton.addEventListener('click', function(){
+    let paragraph = document.createElement('p');
+    paragraph.classList.add('paragraph-styling');
+    pp.appendChild(paragraph);
+    paragraph.innerHTML = inputText.value;
+    inputText.value = "";
+
+    paragraph.addEventListener('click', function(){
+        paragraph.style.textDecoration = 'line-through';
+    });
+
+    paragraph.addEventListener('dblclick', function(){
+        p.removeChild(paragraph);
+    });
+
+    clearToDo.addEventListener('click', function(){
+        paragraph.remove();
+    })
+
+})
